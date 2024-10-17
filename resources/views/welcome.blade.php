de0da5d33e2892eb5ebac3f4b7b4b5aedb9369fa
Peter Kota <kotapeter@gmail.com>
2024-10-17 14:37:33 +0200

    update sevalla block

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,6 +133,7 @@
                                     <h2 class="text-xl font-semibold text-black dark:text-white">Sevalla.com</h2>
 
                                     <p class="mt-4 text-sm/relaxed">Laravel ❤️ Sevalla</p>
+                                    <p class="mt-4 text-sm/relaxed">PR preview feature is awesome</p>
                                 </div>
 
                                 <svg class="size-6 shrink-0 self-center stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
